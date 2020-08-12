9a5e96a9b09956f81e614e2ff83d7de6d37147c0
Pedro Perez <pperezm@tec.mx>
2020-08-12 18:48:24 -0500

    test added

---
+++ activity.h
@@ -0,0 +1,48 @@
+// =================================================================
+//
+// File: activity.h
+// Author: Pedro Perez
+// Description: This file contains the implementation of functions 
+//				of this activity.
+//
+// Copyright (c) 2020 by Tecnologico de Monterrey.  
+// All Rights Reserved. May be reproduced for any non-commercial 
+// purpose.
+// =================================================================
+#ifndef ACTIVITY_H
+#define ACTIVITY_H
+ 
+// =================================================================
+// sumaIterativa. Calculate the sum from 1 to n with an iterative 
+// method.
+// 
+// @param n	The maximum limit of the sum.
+// @return	The result of the addition from 1 to n.
+// =================================================================
+unsigned int sumaIterativa(unsigned int n) {
+	return 0;
+}
+
+// =================================================================
+// sumaRecursiva. Calculate the sum from 1 to n with an recursive 
+// method.
+// 
+// @param n	The maximum limit of the sum.
+// @return	The result of the addition from 1 to n.
+// =================================================================
+unsigned int sumaRecursiva(unsigned int n) {
+	return 0;
+}
+
+// =================================================================
+// sumaRecursiva. Calculate the sum from 1 to n with a mathematical 
+// method.
+// 
+// @param n	The maximum limit of the sum.
+// @return	The result of the addition from 1 to n.
+// =================================================================
+unsigned int sumaDirecta(unsigned int n) {
+	return 0;
+}
+
+#endif /* ACTIVITY_H */
