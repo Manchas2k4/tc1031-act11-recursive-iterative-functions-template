4170ae02e8a1542fbee147f191b302546d5e5038
Pedro Perez <pperezm@tec.mx>
2020-08-25 12:56:39 -0500

    final test

--- activity.cpp
+++ activity.cpp
@@ -2,12 +2,12 @@
 //
 // File: factorial.h
 // Author: Pedro Perez
-// Description: This file contains the test cases for the functions 
-//				of this activity have to pass. IMPORTANT: this file 
+// Description: This file contains the test cases for the functions
+//				of this activity have to pass. IMPORTANT: this file
 //				should not be modified.
 //
-// Copyright (c) 2020 by Tecnologico de Monterrey.  
-// All Rights Reserved. May be reproduced for any non-commercial 
+// Copyright (c) 2020 by Tecnologico de Monterrey.
+// All Rights Reserved. May be reproduced for any non-commercial
 // purpose.
 // =================================================================
 
@@ -19,7 +19,7 @@ TEST(Activity11, IterativeSum) {
 	EXPECT_EQ(55, sumaIterativa(10));
 	EXPECT_EQ(1275, sumaIterativa(50));
 	EXPECT_EQ(5050, sumaIterativa(100));
-	EXPECT_EQ(125250, sumaIterativa(50));
+	EXPECT_EQ(125250, sumaIterativa(500));
 }
 
 TEST(Activity11, RecursiveSum) {
@@ -27,17 +27,17 @@ TEST(Activity11, RecursiveSum) {
 	EXPECT_EQ(55, sumaRecursiva(10));
 	EXPECT_EQ(1275, sumaRecursiva(50));
 	EXPECT_EQ(5050, sumaRecursiva(100));
-	EXPECT_EQ(125250, sumaRecursiva(50));
+	EXPECT_EQ(125250, sumaRecursiva(500));
 }
- 
+
 TEST(Activity11, DirectSum) {
 	EXPECT_EQ(1, sumaDirecta(1));
 	EXPECT_EQ(55, sumaDirecta(10));
 	EXPECT_EQ(1275, sumaDirecta(50));
 	EXPECT_EQ(5050, sumaDirecta(100));
-	EXPECT_EQ(125250, sumaDirecta(50));
+	EXPECT_EQ(125250, sumaDirecta(500));
 }
- 
+
 int main(int argc, char **argv)  {
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
